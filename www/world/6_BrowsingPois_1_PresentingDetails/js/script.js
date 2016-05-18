dd46d3872242b8945c2d492da56b731718df9977
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2016-05-18 22:10:40 +0200

    navigation

---
+++ www/world/6_BrowsingPois_1_PresentingDetails/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Marijn on 18/05/16.
+ */
+$(document).ready(function () {
+    $('.mainmenu_button').click(function () {
+        $('.mainmenu').toggleClass("slidein");
+    });
+});
